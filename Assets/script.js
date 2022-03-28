c4e706ad61b4391f871424e1d1429ddc3c6ac822
alexarizona00 <82853759+alexarizona00@users.noreply.github.com>
2022-03-27 23:45:06 -0700

    css, html completed. js to follow.

---
+++ Assets/script.js
@@ -0,0 +1,9 @@
+fetch('https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=alerts&appid=f9b6ea43279bd22f926c1e54b46af35a')
+  .then(response => response.json())
+  .then(data => console.log(data));
+ 
+
+
+
+  fetch
+  console.log()
