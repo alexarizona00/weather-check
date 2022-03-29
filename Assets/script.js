bc84a8991966908ed9c53728ff56ed76d740254c
alexarizona00 <82853759+alexarizona00@users.noreply.github.com>
2022-03-28 21:33:46 -0700

    figured out a loop to assign the 5 day forecast.. fixed the display values and setting the date

--- Assets/script.js
+++ Assets/script.js
@@ -5,10 +5,11 @@ var tempnow = document.querySelector('#tempnow');
 var windnow = document.querySelector('#windnow');
 var humidnow = document.querySelector('#humidnow');
 var UVNow = document.querySelector('#UVnow');
-
+var currentCard = document.querySelector('#currentcard');
+var fiveDay = document.querySelector('#fivedayforecast');
+let maincol = document.querySelector('#maincol')
 var city = '';
-
-
+var currentTime = '';
 
 searchButton.addEventListener('click', function () {
   city = searchText.value
@@ -17,16 +18,42 @@ searchButton.addEventListener('click', function () {
   fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + '&units=imperial&appid=f9b6ea43279bd22f926c1e54b46af35a')
     .then(response => response.json())
     .then(data => {
-      console.log(data)
-      console.log('main', data.main)
-      console.log(data.wind)
+      console.log(data.name)
+      lat = data.coord.lat;
+      long = data.coord.lon;
       cityheader.textContent = data.name
-      tempnow.textContent = "Temperature: " + data.main.temp  + ' Farenheit'
-      windnow.textContent = "Wind Speed: " + data.wind.speed + 'mph'
-      humidnow.textContent = "Humidity: " + data.main.humidity + "%"
+      currentCard.setAttribute('style', 'display:block')
+      maincol.setAttribute('style', 'display:block')
 
+
+
+      console.log(lat)
+      console.log(long)
+      fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + data.coord.lat + '&lon=' + data.coord.lon + '&exclude=alerts&units=imperial&appid=f9b6ea43279bd22f926c1e54b46af35a')
+        .then(response => response.json())
+        .then(data => {
+          console.log(data)
+
+
+
+
+
+          tempnow.textContent = "Temperature: " + data.current.temp + ' Farenheit'
+          windnow.textContent = "Wind Speed: " + data.current.wind_speed + 'mph'
+          humidnow.textContent = "Humidity: " + data.current.humidity + "%"
+          UVNow.textContent = "UV Index : " + data.current.uvi
+
+          for (i = 1; i < 6; i++) {
+            console.log(data.daily[i])
+            console.log("date" + i)
+            currentTime = moment.unix(data.daily[i].dt).format("MM/DD/YYYY");
+            document.querySelector("#date" + i).textContent = currentTime
+            document.querySelector("#temp" + i).textContent = 'Temp: ' + data.daily[i].temp.day + 'F'
+            document.querySelector('#humid' + i).textContent = 'Humidity: ' + data.daily[i].humidity + '%'
+            document.querySelector('#wind' + i).textContent = 'Wind Speed: ' + data.daily[i].wind_speed + "MPH"
+
+          }
+        })
     })
+
 })
-
-
-
